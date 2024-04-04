3d8e711a72275e1b8f3c855385f10f1ebb9d371b
Shriti Chandra <shrchandra@microsoft.com>
2024-04-04 10:30:27 +0530

    Integration
    Signed-off-by: Shriti Chandra <shrchandra@microsoft.com>

--- src/empdirectory.js
+++ src/empdirectory.js
@@ -6,6 +6,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import axios from 'axios';
+import {ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const EmpDirectory = () => {
 
@@ -15,12 +17,14 @@ const EmpDirectory = () => {
     const handleShow = () => setShow(true);
     const [checked, setChecked] = useState(false);
 
+    const [id, setId] = useState('');
     const [name, setName] = useState('');
     const [department, setDepartment] = useState('');
     const [dateofjoining, setDateofjoining] = useState('');
     const [photofilename, setPhotofilename] = useState('');
     const [isactive, setIsactive] = useState(false);
 
+    const [editid, setEditId] = useState('');
     const [editname, setEditname] = useState('');
     const [editdepartment, setEditdepartment] = useState('');
     const [editdateofjoining, setEditdateofjoining] = useState('');
@@ -47,17 +51,109 @@ const EmpDirectory = () => {
 
     const handleEdit = (id) => {
         handleShow();
+        axios.get(`http://localhost:24654/api/Employee/${id}`)
+        .then((result) => {
+            setEditname(result.data.name);
+            setEditdepartment(result.data.department);
+            setEditdateofjoining(result.data.dateofjoining);
+            setIseditactive(result.data.iseditactive);
+            setEditId(result.data.id);
+        }).catch((error) => {
+            toast.error(error);
+        })
     }
 
 
     const handleDelete = (id) => {
         if (window.confirm("This will delete the employee record. Are you sure?") === true) {
-            alert(id);
+            axios.delete(`http://localhost:24654/api/Employee/${id}`)
+            .then((result)=>{
+                if (result.status == 200){
+                    toast.success('Employee record has been deleted');
+                    getData();
+                }
+            }).catch((error) => {
+                toast.error(error);
+            })
         }
+    }
+
+    const handleSave = () => {
+        const url = 'http://localhost:24654/api/Employee';
+        const data = {
+            "id" : id,
+            "name" : name,
+            "department" : department,
+            "dateofjoining" : dateofjoining,
+            "isactive" : isactive,
+        }
+
+        axios.post(url, data)
+        .then ((result) => {
+            getData();
+            clear();
+            toast.success('Employee record has been added');
+        }).catch((error) => {
+            toast.error(error);
+        })
+    }
+
+    const handleUpdate = () => {
+        const url = `http://localhost:24654/api/Employee/${editid}`;
+        const data = {
+            "id" : editid,
+            "name" : editname,
+            "department" : editdepartment,
+            "dateofjoining" : editdateofjoining,
+            "isactive" : iseditactive,
+        }
+
+        axios.put(url, data)
+        .then((result) => {
+            handleClose();
+            getData();
+            clear(); 
+            toast.success('Employee record has been updated');
+        }).catch((error) => {
+            toast.error(error);
+        })
+    }
+
+    const handleActiveChange = (e) => {
+        if (e.target.checked) {
+            setIsactive(1);
+        }
+        else {
+            setIsactive(0);
+        }
+    }
+
+    const HandleIseditactive = (e) => {
+        if (e.target.checked) {
+            setIseditactive(1);
+        }
+        else {
+            setIseditactive(0);
+        } 
+    }
+
+    const clear = () => {
+        setName('');
+        setDepartment('');
+        setDateofjoining('');
+        setIsactive(0);
+        setPhotofilename('');
+
+        setEditname('');
+        setEditdepartment('');
+        setEditdateofjoining('');
+        setEditPhotofilename('');
+        setIseditactive(0);
     }
 
     return (
         <Fragment>
+            <ToastContainer />
             <Container style={{ marginTop: '20px' }}>
                 <Row >
                     <Col>
@@ -73,11 +169,13 @@ const EmpDirectory = () => {
                             value={dateofjoining} onChange={(e) => setDateofjoining(e.target.value)} />
                     </Col>
                     <Col>
-                        <input type="checkbox" onChange={(e) => setIsactive(e)} value={isactive} />
+                        <input type="checkbox" 
+                        checked = {isactive  === 1 ? true : false}
+                        onChange={(e) => handleActiveChange(e)} value={isactive} />
                         <label>Is this an active record?</label>
                     </Col>
                     <Col>
-                        <Button variant="primary" color="success">Submit</Button>
+                        <Button variant="primary" color="success" onClick={() => handleSave()}>Submit</Button>
                     </Col>
                 </Row>
             </Container>
@@ -123,19 +221,21 @@ const EmpDirectory = () => {
                     <Container>
                         <Row>
                             <Col>
-                                <input type="text" className="form-control" placeholder="Employee Name"
+                                <input type="text" className="form-control" placeholder={name}
                                     value={editname} onChange={(e) => setEditname(e.target.value)} />
                             </Col>
                             <Col>
-                                <input type="text" className="form-control" placeholder="Employee Department"
+                                <input type="text" className="form-control" placeholder={department}
                                     value={editdepartment} onChange={(e) => setEditdepartment(e.target.value)} />
                             </Col>
                             <Col>
-                                <input type="date" className="form-control" placeholder="Date of Joining"
+                                <input type="date" className="form-control" placeholder={dateofjoining}
                                     value={editdateofjoining} onChange={(e) => setEditdateofjoining(e.target.value)} />
                             </Col>
                             <Col>
-                                <input type="checkbox" onChange={(e) => setIseditactive(e)} value={iseditactive} />
+                                <input type="checkbox" 
+                                checked = {iseditactive  === 1 ? true : false}
+                                onChange={(e) => HandleIseditactive(e)} value={iseditactive} />
                                 <label>Is this an active record?</label>
                             </Col>
                         </Row>
@@ -145,7 +245,7 @@ const EmpDirectory = () => {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={handleUpdate}>
                         Save Changes
                     </Button>
                 </Modal.Footer>
